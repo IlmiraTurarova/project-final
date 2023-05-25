b773a0735004047a0783ebbba8d3e639042d05be
IlmiraTurarova <ilmira.rita@gmail.com>
2023-05-26 01:41:22 +0600

    Fix: remove liquibase from application-test.yaml, remove vk from test.sql

--- src/test/java/com/javarush/jira/bugtracking/DushBoardUIControllerTest.java
+++ src/test/java/com/javarush/jira/bugtracking/DushBoardUIControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Sql(scripts = "classpath:db/test.sql", config = @SqlConfig(encoding = "UTF-8"))
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class DashBoardUIControllerTest {
